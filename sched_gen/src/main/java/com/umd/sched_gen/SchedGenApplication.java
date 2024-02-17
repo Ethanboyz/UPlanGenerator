fb9cd475982e8e49bca46849e7c6bab24268572b
Ethan Doan <ethanboyz10@gmail.com>
2024-02-16 22:18:16 -0500

    Spring boot framework

---
+++ sched_gen/src/main/java/com/umd/sched_gen/SchedGenApplication.java
@@ -0,0 +1,14 @@
+package com.umd.sched_gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedGenApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ sched_gen/src/test/java/com/umd/sched_gen/SchedGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umd.sched_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
